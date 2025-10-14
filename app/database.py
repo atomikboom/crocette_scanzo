973f4dce19af3d786ce827db79af4179faaa9da2
atomikboom <90612982+atomikboom@users.noreply.github.com>
2025-10-14 16:58:58 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,18 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./crocette.db")
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+# Leggi la connessione dal pannello di Render (che la prenderà da Neon)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=False, future=True, connect_args=connect_args)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
+# Se vuoi fare test in locale:
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./local.db")
 
-class Base(DeclarativeBase):
-    pass
+# Crea engine SQLAlchemy
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-from typing import Generator
-def get_db() -> Generator:
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
     db = SessionLocal()
     try:
         yield db
